d8b12151893d83575df83eada6a07ab7a2f0f28e
Kiran Yadav <Kiran_Yadav@mckinsey.com>
2022-03-23 21:17:07 +0530

    feat:add Homepage styling

--- tweety/src/pages/HomePage/index.jsx
+++ tweety/src/pages/HomePage/index.jsx
@@ -1,11 +1,26 @@
-import './HomePage.css';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './HomePage.css';
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
-    <div className="home-page">
-      Welcome to Tweety
+    <div className="root-container">
+      <p className="hompage-main-para"> Welcome to Tweety</p>
+
+      <button
+        className="sync-button"
+        type="submit"
+        onClick={() => {
+          navigate('/records');
+        }}
+
+      >
+        users
+      </button>
+
     </div>
+
   );
 }
 export default HomePage;
