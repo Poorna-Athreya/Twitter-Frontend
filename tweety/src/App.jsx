4e784d90bc25b8697f371fe0ccd55891e2631c08
Poorna-Athreya <poorna_athreya@mckinsey.com>
2022-03-23 15:10:20 +0530

    Feat: Starting page, not found page skeletons

--- tweety/src/App.jsx
+++ tweety/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { USERS_ROUTE, TWEETS_ROUTE } from './components/constants/routes';
+import { HomePage, NotFoundPage } from './pages';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={USERS_ROUTE} element={<div />} />
+          <Route path={`${USERS_ROUTE}/:userId${TWEETS_ROUTE}`} element={<div />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ tweety/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="header">
+      Tweety
+    </div>
+  );
+}
+export default Header;
