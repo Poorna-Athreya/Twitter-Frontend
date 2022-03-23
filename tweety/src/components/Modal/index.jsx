b2b2788abd14b5c79f7f16fa625a8e948b1fb97c
Lavanya <LAVANYA_A_M@mckinsey.com>
2022-03-23 20:08:28 +0530

    refactor: style modal popup component

--- tweety/src/components/Modal/index.jsx
+++ tweety/src/components/Modal/index.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import './Modal.css';
 
 export default function Modal({ setShowModal }) {
   return (
     <div className="modal">
       <button type="button" onClick={() => setShowModal(false)}>X</button>
-      <h2>Add New Tweet</h2>
-      <input type="text" />
-      <button type="submit">Post</button>
+      <div className="new-tweet-box">
+        <h3>Add New Tweet</h3>
+        <br />
+        <input type="text" className="new-tweet-text-box" />
+        <br />
+        <br />
+        <button type="submit" className="post-new-tweet">Post</button>
+      </div>
     </div>
   );
 }
