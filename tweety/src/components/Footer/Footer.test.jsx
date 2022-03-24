087bbed51f9cf85af1ba69d81aa9b1c1c02c6bc4
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-24 08:13:07 +0530

    feat: add test to footer, header and loader

---
+++ tweety/src/components/Footer/Footer.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Footer from './index';
+
+describe('Footer', () => {
+  it('should have the required elements when Footer is rendered', () => {
+    render(<Footer />);
+    expect(screen.queryByTestId('footerText')).toBeTruthy();
+    expect(screen.getByText('copyright@2022')).toBeTruthy();
+  });
+});
--- tweety/src/components/Header/index.jsx
+++ tweety/src/components/Header/index.jsx
@@ -6,8 +6,8 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 function Header() {
   return (
     <header className="header">
-      <h1 className="tweety-icon"><FontAwesomeIcon icon={faTwitter} /></h1>
-      <h1 className="tweety-heading">Tweety</h1>
+      <h1 data-testid="headerIcon" className="tweety-icon"><FontAwesomeIcon icon={faTwitter} /></h1>
+      <h1 data-testid="headerHeading" className="tweety-heading">Tweety</h1>
     </header>
   );
 }
